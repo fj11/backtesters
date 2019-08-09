4c0b205f840d62e97ecd4ed978e2e223d4af2072
fengjian <fengjian1114@gmail.com>
2019-08-08 23:16:09 -0400

    add update freqency tab

--- libs/windows/coding/__init__.py
+++ libs/windows/coding/__init__.py
@@ -2,9 +2,8 @@
 
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtWidgets import \
-    QMdiSubWindow, QTabWidget, QAction
+    QMdiSubWindow
 from PySide2.QtCore import Qt
-from src import setting
 
 class CodingWidget():
 
@@ -13,8 +12,6 @@ class CodingWidget():
         subWindow = QMdiSubWindow()
         loader = QUiLoader()
         python_editor = loader.load('coding.ui', parentWidget=parent_widget)
-
-
 
         subWindow.setWidget(python_editor)
         parent.mdi_area.addSubWindow(subWindow)
