5c1dca71add01784b4d76936229c863070416c9f
fengjian <fengjian1114@gmail.com>
2019-09-02 22:56:28 -0400

    update coding page

--- src/functions.py
+++ src/functions.py
@@ -9,7 +9,17 @@ functions_name = {
         "add_signal":{
             "name": "add_signal",
             "demo_code": "# 添加信号列\n\nresult = read_data('50ETF', 0)\nprint(result)\n"
-        }
+        },
+
+        "get_stock_ids":{
+            "name": "get_stock_ids",
+            "demo_code": "# 获取所有股票列表\n\nresult = get_stock_ids()\nprint(result)\n"
+        },
+
+        "get_stock":{
+            "name": "get_stock",
+            "demo_code": "# 获取股票实例\n\nstock_id = get_stock_ids()\nstock_item = get_stock(stock_id[1])\nprint(stock_item.daily.text)\n"
+        },
 
 
     }
